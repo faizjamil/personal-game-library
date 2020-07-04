09c48a351453da2a23416cb4878896efeff3a35a
Faizan Jamil <faizan.jamilwork@gmail.com>
2020-07-03 22:14:39 -0400

    added basic framework for express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-console.log("Hello world")
-console.log("")
+const express = require('express')
+const port = 3000
+const app = express()
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
