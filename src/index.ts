10c17edb435ad75dfe30bf5bc7aca14983422580
Faizan Jamil <faizan.jamilwork@gmail.com>
2020-07-09 14:58:04 -0400

    made the placeholder route look a bit cleaner

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import express from 'express';
+import express, { Application } from 'express';
 const port = 3000;
-const app = express();
-app.get('/', (req: express.Request, res: express.Response) => res.send('Hello World!'));
+const app: Application = express();
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('Hello World!')
+});
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
