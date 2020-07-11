5bf525f044573c633e33100e9041d49c584cd1e4
Faizan Jamil <faizan.jamilwork@gmail.com>
2020-07-11 16:09:40 -0400

    changed target to es2017

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ const port = 3000;
 const app: Application = express();
 app.get('/', (req: express.Request, res: express.Response) => {
     res.send('Hello World!')
+    res.status(200);
 });
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
