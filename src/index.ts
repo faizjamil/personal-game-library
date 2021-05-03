1a17988fa966676c5e5471a62e266581589f411c
Faizan <faizan.jamilwork@gmail.com>
2021-05-02 21:45:53 -0400

    added some comments and packages

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ const app: Application = express();
 app.use('/users', userRouter);
 app.get('/', (req: express.Request, res: express.Response) => {
     res.send('Hello World!')
+    
     res.status(200);
+    
 });
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => 
+    console.log(`Example app listening at http://localhost:${port}`)
+
+);
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,6 +3,8 @@ import controller from '../controllers/user'
 
 const router: Router = express();
 
+// functions for each route are handled by the user controller
+
 router.get('/register', controller.register);
 router.get('/login', controller.login);
 router.get('/read', controller.read);
