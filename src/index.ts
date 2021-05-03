71c43bb1c7578dbcda9ed2a1369412b6f4a03eb0
Faizan <faizan.jamilwork@gmail.com>
2021-05-02 21:59:14 -0400

    added environment variable for port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Application } from 'express';
 import userRouter from './routes/user'
-const port = 3000;
+const port = 3000 || process.env.PORT;
 const app: Application = express();
 app.use('/users', userRouter);
 app.get('/', (req: express.Request, res: express.Response) => {
