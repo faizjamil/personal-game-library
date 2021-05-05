65d67625e6d4433acce7cc894c8dc35318dbb854
Faizan <faizan.jamilwork@gmail.com>
2021-05-04 21:56:55 -0400

    made use of ts-node-dev to replace nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express, { Application } from 'express';
 import userRouter from './routes/user'
+
 const port = 3000 || process.env.PORT;
 const app: Application = express();
 app.use('/users', userRouter);
 app.get('/', (req: express.Request, res: express.Response) => {
-    res.send('Hello World!')
+    res.send('Hello World! test')
     
     res.status(200);
     
